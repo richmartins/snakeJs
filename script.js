6af2e6b03d78b460f3a9fd5f0c64a34cdb155224
richmartins <richard.martinstenorio@gmail.com>
2020-05-26 19:16:23 +0200

    [DONE] some sort of gui and start the game when cliked on start

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
+/*** Made by Hadrien Louis and Richard Tenorio ***/
+/*** based on the video of blablaba.... ***/
+
+/*** INIT VARS  ***/
+score = 1;
+speed = 6;
+unit=gs=20;
+tc=document.getElementById('playground').width / unit;
+
+// init the snake's tail that increase each time we step in an apple
+trail=[];
+tail=5;
+px=py=Math.floor(tc / 2);
+
+//init apple position
+ax=ay=Math.floor(Math.random() * tc)
+
+// init x's axe volecity and the y's velocity
+xv=yv=0;
+
+// modal
+document.getElementById("gui").onclick = function () {
+    this.style.display = "none";
+    document.addEventListener("keydown", keyPush);
+    document.getElementById('up').addEventListener("touchend", mobileControls);
+    document.getElementById('down').addEventListener("touchend", mobileControls);
+    document.getElementById('left').addEventListener("touchend", mobileControls);
+    document.getElementById('right').addEventListener("touchend", mobileControls);
+    scoreCont = document.getElementById("score");
+
+    // setting the frame rate of the game
+    // because snake like game goes on low frame rate. We call the game function 15 times a second
+    setInterval(game, 1000/speed);
+}
+
 window.onresize=function(){
-        responsive();
-    }
+    responsive();
+}
 
-    window.onload=function(){
-        responsive();
+window.onload=function(){
+    responsive();
 
-        pg = document.getElementById("playground");
-        pg.height = pg.width
+    pg = document.getElementById("playground");
+    pg.height = pg.width
 
-        ctx = pg.getContext('2d');
-        document.addEventListener('touchmove', function (event) {
-            event = event.originalEvent || event;
-            if (event.scale > 1) {
-                event.preventDefault();
-            }
-        }, false);
-        document.addEventListener("keydown", keyPush);
-        document.getElementById('up').addEventListener("touchend", mobileControls);
-        document.getElementById('down').addEventListener("touchend", mobileControls);
-        document.getElementById('left').addEventListener("touchend", mobileControls);
-        document.getElementById('right').addEventListener("touchend", mobileControls);
-        scoreCont = document.getElementById("score");
+    ctx = pg.getContext('2d');
+    document.addEventListener('touchmove', function (event) {
+        event = event.originalEvent || event;
+        if (event.scale > 1) {
+            event.preventDefault();
+        }
+    }, false);
+}
 
-        // setting the frame rate of the game
-        // because snake like game goes on low frame rate. We call the game function 15 times a second
-        setInterval(game, 1000/speed);
-    }
-    score = 1;
-    speed = 15;
-    unit=gs=20;
-	tc=document.getElementById('playground').width / unit;
-	
-    // init the snake's tail that increase each time we step in an apple
-    trail=[];
-    tail=5;
-    
-    px=py=Math.floor(tc / 2);
-
-    //init apple position
-    ax=ay=Math.floor(Math.random() * tc)
-
-    // init x's axe volecity and the y's velocity
-    xv=yv=0;
 
     function mobileControls(event){
         var keyCode;
@@ -68,29 +78,30 @@ window.onresize=function(){
         switch(event.keyCode){
             case 37:			// 37 and the others cases is the keycode when we press it
                 if (xv != 1) {
-					xv=-1;yv=0;
-				}
-				 // changing the direction of the velocity will obviously change the direction where the snake goes
+                    xv=-1;yv=0;
+                }
+                 // changing the direction of the velocity will obviously change the direction where the snake goes
                 break;
              case 38:
-				if (yv != 1) {
-					xv=0;yv=-1;
-				}
+                if (yv != 1) {
+                    xv=0;yv=-1;
+                }
                 break;
             case 39:
-				if (xv != -1) {
-					xv=1;yv=0;
-				}
+                if (xv != -1) {
+                    xv=1;yv=0;
+                }
                 break;
             case 40:
-				if (yv != -1) {
-					xv=0;yv=1;
-				}
+                if (yv != -1) {
+                    xv=0;yv=1;
+                }
                 break;
         }
     }
 
     function game(event){
+
         // player x and player y position depends on the x's and y's velocity
         px+=xv;
         py+=yv;
@@ -139,7 +150,7 @@ window.onresize=function(){
         // when the player steps into an apple we increase the snake tail
         // and make a new apple in a random position
         if(ax==px && ay==py){
-            setInterval(game, 1000/(speed += 1))
+            setInterval(game, 1000/(speed *= 0.5))
             tail++;
             score++;
             scoreCont.innerHTML="Score: " + score;
